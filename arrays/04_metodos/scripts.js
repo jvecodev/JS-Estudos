6f292438f1736772ddc0a2f79b8846ad4c1777cd
jvecodev <jvitor.oliveira1803@gmail.com>
2025-08-20 18:17:17 -0300

    feat(arrays): adicionar exemplos de manipulação de arrays e objetos em JavaScript

---
+++ arrays/04_metodos/scripts.js
@@ -0,0 +1,13 @@
+let marca = 'nike';
+
+console.log(marca.toUpperCase());
+
+let marca2 = marca.toUpperCase();
+
+console.log(marca2.toLowerCase());
+
+// A respota deve vir como function
+console.log(typeof marca2.toLowerCase);
+
+
+
